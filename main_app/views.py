901cfcda40458fece3d3fcc541253c3b84d08a23
RJ Crowder-Schaefer <rj.crowder@gmail.com>
2023-06-28 22:43:54 -0400

    Created html template files, static css and js files and began to build out homepage

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from django.views import View
+from django.http import HttpResponse
 
 # Create your views here.
+
+# class Home(View):
+
+#     def get(self, request):
+#         return HttpResponse("Best Places To Visit 2023")
+    
+
+# class About(View):
+
+#     def get(self, request):
+#         return HttpResponse("Best Places About")
+    
+# class PlacesList(View):
+
+#     def get(self, request):
+#         return HttpResponse("Best Places List")
 
 class Home(TemplateView):
     template_name = "home.html"
@@ -9,5 +27,5 @@ class Home(TemplateView):
 class About(TemplateView):
     template_name = "about.html"
 
-class PlaceList(TemplateView):
-    template_name = "place_list.html"
+# class PlaceList(TemplateView):
+#     template_name = "place_list.html"
